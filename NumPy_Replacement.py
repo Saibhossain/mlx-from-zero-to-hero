e11655969b50f9b8cdba0ae91039f438b709a06a
mdsaibhossain <saibhossain5@gmail.com>
2025-09-14 23:11:43 +0600

    numpy

--- NumPy_Replacement.py
+++ NumPy_Replacement.py
@@ -4,4 +4,5 @@ a = mx.array([[1.0, 2.0], [3.0, 4.0]])
 b = mx.array([[5.0, 6.0], [7.0, 8.0]])
 
 print("Matrix Addition:\n", a + b)
+print("Matrix subtraction:\n", a - b)
 print("Matrix Multiplication:\n", mx.matmul(a, b))
